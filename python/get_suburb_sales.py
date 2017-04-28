d867f417bd635a99b5720e9f0fc6346e3d03b729
Alex <alexlewis@hotmail.com.au>
2017-04-28 16:11:33 +1000

    create and run script to get suburb csv

---
+++ python/get_suburb_sales.py
@@ -0,0 +1,34 @@
+import urllib
+import os
+import csv
+
+CWD = os.getcwd()
+
+
+def prep_name(suburb_human):
+    """Convert human readable suburb name and convert for url."""
+    s = suburb_human.upper()
+    s = str.split(s)
+    s = '%20'.join(s)
+    return s
+
+
+def save_csv(suburb, postcode):
+    """Download and save the csv.
+
+    Save from NSW government website based on list of
+    suburbs file.
+    """
+    base_url = "http://maps.six.nsw.gov.au/csv/current/suburb/"
+    full_url = base_url + prep_name(suburb) + "_" + str(postcode) + ".csv"
+
+    save_path = CWD + "/data/" + suburb + ".csv"
+    save = urllib.URLopener()
+    save.retrieve(full_url, save_path)
+    return save_path
+
+
+with open("data/suburbs_list.csv") as csvfile:
+    suburb_list = csv.reader(csvfile, delimiter=",")
+    for row in suburb_list:
+        print save_csv(row[0], row[1])
